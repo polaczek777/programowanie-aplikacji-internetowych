0bb42384c874fb0900380282f350575d658e084d
polaczek777 <grzp31@gmail.com>
2023-10-04 13:11:58 +0200

    sesja

--- session2.php
+++ session2.php
@@ -1,9 +1,9 @@
 <?php
 session_start();
-echo "welcome to page #2";
+echo "welcome to page #2".'<br>';
 
 echo 'ulubiony kolor: '.$_SESSION['favcolor'];
-echo 'ulubione zwierzę: '.$_SESSION['animal'].'<br>';
+echo ' ulubione zwierzę: '.$_SESSION['animal'].'<br>';
 echo date('Y m d H:i:s',$_SESSION['time']).'<br>';
 
 echo $_SESSION['bzdura'];
