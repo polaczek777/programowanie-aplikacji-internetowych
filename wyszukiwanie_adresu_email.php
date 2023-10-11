888fd0657971df3cdf02fe230740d539455064e0
polaczek777 <grzp31@gmail.com>
2023-10-11 13:02:05 +0200

    wyszukiwanie_adresu_email

---
+++ wyszukiwanie_adresu_email.php
@@ -0,0 +1,8 @@
+<?php
+$wzorzec='/[-\w.]+@([-A-z0-9]+\.)+[A-z]/';
+$tekst="SeatTopAudiGniot100@wp.pl";
+if(preg_match($wzorzec,$tekst,$matches))
+echo "znaleziono email".$matches[0];
+else
+echo "nie znaleziono email";
+?>
