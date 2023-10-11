d4868c0808f3306597dc509b2071d201e50ff7c5
polaczek777 <grzp31@gmail.com>
2023-10-11 13:14:48 +0200

    wyszukiwanie_adresu_email

--- wyszukiwanie_adresu_email.php
+++ wyszukiwanie_adresu_email.php
@@ -2,7 +2,7 @@
 $wzorzec='/[-\w.]+@([-A-z0-9]+\.)+[A-z]/';
 $tekst="SeatTopAudiGniot100@wp.pl";
 if(preg_match($wzorzec,$tekst,$matches))
-echo "znaleziono email".$matches[0];
+echo "znaleziono email ".$matches[0];
 else
-echo "nie znaleziono email";
+echo "nie znaleziono email ";
 ?>
