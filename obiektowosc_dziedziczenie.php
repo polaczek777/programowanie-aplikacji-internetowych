023912d4758c2b4ae29b157c5973880141946088
polaczek777 <grzp31@gmail.com>
2023-10-18 13:52:47 +0200

    obiektownosc_dziedziczenie

--- obiektowosc_dziedziczenie.php
+++ obiektowosc_dziedziczenie.php
@@ -1,13 +1,23 @@
 <?php
 class Fruit2{
-    public $name;
-    public $color;
-    public function __construct($name,$color){
-        $this->name=$name;
-        $this->color=$color;
-    }
-    protected function intro(){
-        echo "this fruit is {$this->name} and the color is {$this->color}.";
-    }
+  public $name;
+  public $color;
+  public function __construct($name, $color) {
+    $this->name = $name;
+    $this->color = $color;
+  }
+  public function intro() {
+    echo "The fruit is {$this->name} and the color is {$this->color}.";
+  }
 }
+
+// Strawberry is inherited from Fruit
+class Strawberry extends Fruit {
+  public function message() {
+    echo "Am I a fruit or a berry? ";
+  }
+}
+$strawberry = new Strawberry("Strawberry", "red");
+$strawberry->message();//ok
+$strawberry->intro();//error
 ?>
