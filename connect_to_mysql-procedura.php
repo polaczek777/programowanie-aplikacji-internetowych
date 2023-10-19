b9c67d3b31af9f1ac7545f4e5bae05bf2a7c87ff
polaczek777 <grzp31@gmail.com>
2023-10-19 08:03:08 +0200

    procedura_łączenia_mysql

---
+++ connect_to_mysql-procedura.php
@@ -0,0 +1,11 @@
+<?php
+$servername="localhost";
+$username="root";
+$password="";
+
+$conn=mysqli_connect($servername,$username,$password);
+if(!$conn){
+    die("connection failed: ".mysqli_connect_error());
+}
+echo "connected successfully"
+?>
