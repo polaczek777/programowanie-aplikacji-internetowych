46ddb1906dac028c45fad722973bb583520b0646
polaczek777 <grzp31@gmail.com>
2023-10-19 09:48:56 +0200

    select_data_mysql

---
+++ select_data_mysql.php
@@ -0,0 +1,45 @@
+<?php
+$servername="localhost";
+$username="root";
+$password="";
+$db="inb5";
+
+$conn=mysqli_connect($servername,$username,$password,$db);
+if(!$conn){
+    die("connection failed: ".mysqli_connect_error());
+}
+echo "connected successfully";
+
+$database="INB5";
+$sql="CREATE DATABASE $database";
+if (mysqli_query($conn,$sql)){
+    echo "database create successfully";
+}else{
+    echo "error crating database: ".mysqli_error($conn);
+}
+$sql="CREATE TABLE MyGuests(
+    id INT(6) UNSIGNED AUTO_INCREMENT
+        PRIMARY KEY,
+    firstname VARCHAR(30) NOT NULL,
+    lastname VARCHAR(30) NOT NULL,
+    email VARCHAR(50),
+    reg_date TIMESTAMP DEFAULT
+            CURRENT_TIMESTAMP ON UPDATE
+        CURRENT_TIMESTAMP
+)";
+if (mysqli_query($conn,$sql)){
+    echo "table MyGuests create successfully";
+}else{
+    echo "error crating table: ".mysqli_error($conn);
+}
+$sql="SELECT id, firstname, lastname FROM MyGuests";
+$result=mysqli_query($conn,$sql);
+if(mysqli_num_rows($result)>0){
+    while($row=mysqli_fetch_assoc($result)){
+        echo "id: ".$row["id"]." - name: ".$row["firstname"]. " ".$row["lastname"]."<br>";
+    }
+}else{
+    echo "0 results";
+}
+mysqli_close($conn);
+?>
