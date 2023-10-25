e2d382c5814ad64ecce52d45dd199f7a12b2e1f1
polaczek777 <grzp31@gmail.com>
2023-10-25 14:04:35 +0200

    Isert_to_mysqli_from_from

---
+++ insert_to_mysqli_from_from.php
@@ -0,0 +1,21 @@
+<?php
+$db=mysqli_connect("localhost","root","","inb5");
+if(!$db){
+die("connection failed: ".mysqli_connect_error());
+}
+echo "connected successfully"."<br>";
+
+$name=$_POST["name"];
+$lastname=$_POST["lastname"];
+$email=$_POST["email"];
+
+$sql="INSERT INTO MyGuests(firstname,lastname,email)
+VALUES('$name', '$lastname', '$email')";
+echo $sql;
+if(mysqli_query($db,$sql)){
+    echo "Dodano rekord";
+}else{
+    echo "Error: ".$sql."<br>".mysqli_error($db);
+}
+mysqli_close($db);
+?>
