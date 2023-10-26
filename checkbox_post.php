7ac43cc79ccddfa96c6f65bc5a5c2a73f718400a
polaczek777 <grzp31@gmail.com>
2023-10-26 09:10:15 +0200

    checkbox_post_formularz1

--- checkbox_post.php
+++ checkbox_post.php
@@ -1,43 +1,36 @@
 <?php
-$servername="localhost";
-$username="root";
-$password="";
-$db="inb5";
-
-$conn=mysqli_connect($servername,$username,$password,$db);
-if(!$conn){
-    die("connection failed: ".mysqli_connect_error());
-}
-echo "connected successfully";
-
-$sql="ALTER TABLE MyGuests ADD vehicle2 VARCHAR(30) NOT NULL";
-if (mysqli_query($conn,$sql)){
-    echo "table MyGuests add successfully";
-}else{
-    echo "error add table: ".mysqli_error($conn)."<br>";
-}
-
-$sql="ALTER TABLE MyGuests ADD vehicle2 VARCHAR(30) NOT NULL";
-if (mysqli_query($conn,$sql)){
-    echo "table MyGuests add successfully";
-}else{
-    echo "error add table: ".mysqli_error($conn)."<br>";
-}
-
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "inb5";
 $name=$_POST["name"];
 $lastname=$_POST["lastname"];
 $email=$_POST["email"];
-$vehicle1=$_POST["vehicle1"];
-$vehicle2=$_POST["vehicle2"];
 
-$sql="INSERT INTO MyGuests(firstname,lastname,email,vehicle,vehicle2)
-VALUES('$name', '$lastname', '$email','$vehicle1','$vehicle2')";
-echo $sql;
-if(mysqli_query($conn,$sql)){
-    echo "Dodano rekord";
-}else{
-    echo "Error: ".$sql."<br>".mysqli_error($conn)."<br>";
+$vehicle1=NULL;
+$vehicle2=NULL;
+
+if(isset($_POST['vehicle'])){
+    $vehicle1=$_POST['vehicle'];
+}
+if(isset($_POST['vehicle2'])){
+    $vehicle2=$_POST['vehicle2'];
 }
 
-mysqli_close($conn);
-?>
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql="INSERT INTO MyGuests (firstname, lastname, email,vehicle,vehicle2)
+VALUES('$name','$lastname','$email','$vehicle1','$vehicle2')";
+
+if (mysqli_query($conn, $sql)) {
+    echo "Dodano rekord";
+  } else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+  }
+
+  mysqli_close($conn);
+  ?>
